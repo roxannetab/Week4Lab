6c2a345a9ce42848745b5dc552df6230c4d2c59c
RT <RT@FLUBB>
2022-10-03 00:05:39 -0600

    New Project Creation Created new application for Week 4 Lab

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -32,7 +32,7 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //gets user inputted values for "title" and "content"
+       
         String saveTitle = request.getParameter("title");
         String saveContent = request.getParameter("content");
         
